51920b1d631ad0a89e9e237913cb44d3fadd13da
Angel Sirodey Alonso <xiro@MacBook-Pro-de-Angel.local>
2017-11-17 11:52:35 +0100

    iteration 5 finish

--- starter-code/app.js
+++ starter-code/app.js
@@ -6,9 +6,13 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const expressLayouts = require('express-ejs-layouts');
 const authRoute = require('./routes/auth')
+const laundryRoutes = require('./routes/laundry');
 const index = require('./routes/index');
 const users = require('./routes/users');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
 const app = express();
 
 const dbURL = 'mongodb://localhost/uberlaundry';
@@ -30,9 +34,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'never do your own laundry again',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
+app.use((req, res, next) => {
+  if (req.session.currentUser) {
+    res.locals.currentUserInfo = req.session.currentUser;
+    res.locals.isUserLoggedIn = true;
+  } else {
+    res.locals.isUserLoggedIn = false;
+  }
+  next();
+});
+
 app.use('/', index);
 app.use('/users', users);
 app.use('/', authRoute);
+app.use('/', laundryRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ starter-code/routes/laundry.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user')
+
+router.get('/dashboard', (req, res, next) => {
+  res.render('laundry/dashboard');
+});
+
+router.post('/launderers', (req, res, next) => {
+    const userId = req.session.currentUser._id;
+    const laundererInfo = {
+      fee: req.body.fee,
+      isLaunderer: true
+    };
+  
+    User.findByIdAndUpdate(userId, laundererInfo, { new: true }, (err, theUser) => {
+      if (err) {
+        next(err);
+        return;
+      }
+  
+      req.session.currentUser = theUser;
+  
+      res.redirect('/dashboard');
+    });
+  });
+
+  router.get('/launderers', (req, res, next) => {
+    User.find({ isLaunderer: true }, (err, launderersList) => {
+      if (err) {
+        next(err);
+        return;
+      }
+  
+      res.render('laundry/launderers', {
+        launderers: launderersList
+      });
+    });
+  });
+  
+module.exports = router;
